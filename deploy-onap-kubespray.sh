10550733dd87a80a6dbe0c75ffd273a40e1ba0f9
Martin Klozik <4032956-p00rman@users.noreply.gitlab.com>
2019-06-05 09:35:49 +0000

    Update deploy-onap-kubespray.sh
    Fix kubectl error suppression.

--- deploy-onap-kubespray.sh
+++ deploy-onap-kubespray.sh
@@ -292,7 +292,7 @@ cd ../../
 
 echo "Waiting for ONAP deployments to be up \$(date)"
 function get_onap_deployments() {
-    sudo -i kubectl get deployments --namespace $NAMESPACE | grep -v ' 0/0 ' | tail -n+2 > $TMP_DEP_LIST 2> /dev/null
+    sudo -i kubectl get deployments --namespace $NAMESPACE 2> /dev/null | grep -v ' 0/0 ' | tail -n+2 > $TMP_DEP_LIST
     return \$(cat $TMP_DEP_LIST | wc -l)
 }
 FAILED_DEPS_LIMIT=0         # maximal number of failed ONAP deployemnts
